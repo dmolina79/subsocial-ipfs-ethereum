3781edbcbdd613c15f072703f74b2300fb08fcb2
olehmell <oleh.melnechyk@gmail.com>
2020-07-23 19:39:54 +0300

    orbitdb initial commit

---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,4 @@
+import { Comments } from '../components/comments/Comments'
+import CommentsProvider from '../components/comments/СommentContext'
+
+export default () => <CommentsProvider><Comments /></CommentsProvider>
