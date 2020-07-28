d889f181e95e3676bbf8c05cbefa2e7bfc6b17ae
olehmell <oleh.melnechyk@gmail.com>
2020-07-28 20:55:23 +0300

    Add css suport

--- ui/src/pages/_app.js
+++ ui/src/pages/_app.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
-import { OrbitDbProvider } from '../components/orbitdb'
 
+import { OrbitDbProvider } from '../components/orbitdb'
+import { PostStoreProvider } from '../components/posts/PostsContext'
+import { BucketProvider } from '../components/buckets/BucketsContext'
+
+import 'antd/dist/antd.css'
 import '../styles/main.less'
 import '../styles/bootstrap-utilities-4.3.1.less'
 import '../styles/ant-override.less'
@@ -15,7 +19,11 @@ function MyApp (props) {
         {/* <script src="/env.js" /> */}
       </Head>
       <OrbitDbProvider>
-          <Component {...pageProps} />
+        <BucketProvider>
+          <PostStoreProvider>
+            <Component {...pageProps} />
+          </PostStoreProvider>
+        </BucketProvider>
       </OrbitDbProvider>
     </>
   )
