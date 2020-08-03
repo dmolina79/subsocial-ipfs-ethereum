eaa1a5ae69826697a342b9bbc9201df1c0c17e40
olehmell <oleh.melnechyk@gmail.com>
2020-08-03 19:22:27 +0300

    Change paths

--- ui/src/pages/spaces/[spaceId]/edit.tsx
+++
@@ -1,4 +0,0 @@
-import dynamic from 'next/dynamic'
-const EditSpace = dynamic(import('../../../components/spaces/SpaceEditor').then(x => x.EditSpace as any), { ssr: false })
-
-export default EditSpace
